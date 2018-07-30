ac6f430a27bbeaf35d3d0985495d0be4407d3802
tonyfromsz <295191928@qq.com>
2018-07-30 20:48:18 +0800

    admin accounts handling

--- api/admin_api.py
+++ api/admin_api.py
@@ -4,7 +4,7 @@ from flask import jsonify
 from api import api
 from app import rpc
 from flask.views import MethodView
-from flask_util import HttpError, RequestData, login_manager, login_required
+from flask_util import HttpError, RequestData, login_manager, login_required, current_user
 from util import is_mobile
 
 
@@ -34,7 +34,9 @@ def login():
         "username": res["username"],
         "id": res["id"],
         "role": res["role"],
-        "createdAt": res["createdAt"]
+        "createdAt": res["createdAt"],
+        "adminRange": res["adminRange"],    # 补货员-补货员id， 场地方-场地id， 品牌方-商品id
+        "adminRange2": res["adminRange2"]   # 品牌方-场地id
     })
     login_manager.login_user(res)
     return resp
@@ -210,6 +212,106 @@ class UserGroupAPI(MethodView):
         return ""
 
 
+@api.route("/admin/test")
+@login_required
+def login_test():
+    # print(type(current_user))
+    print(current_user['username'])
+    # print(dict(current_user))
+    return jsonify(dict(current_user))
+
+
+class AccountAPI(MethodView):
+
+    # @login_required
+    def get(self):
+        rdata = RequestData()
+        accounts = rpc.invbox.get_accounts(page=rdata.page,
+                                           page_size=rdata.page_size)
+        return jsonify(accounts)
+
+    # @login_required
+    def post(self):
+        rdata = RequestData()
+
+        name = rdata.get('name', type=str)
+        mobile = rdata.get('mobile', type=str)
+        password = rdata.get('password', type=str)
+        com_pwd = rdata.get('comPwd', type=str)
+        role = rdata.get('role')
+        admin_range = rdata.get('range')
+        info_list = rdata.get('infoList')
+        if not name:
+            raise HttpError(400, "用户名不能为空")
+        if not is_mobile(mobile):
+            raise HttpError(400, "手机号码格式不正确")
+        if not password == com_pwd:
+            raise HttpError(400, "两次输入密码不一致")
+        if not role:
+            raise HttpError(400, "请输入用户角色")
+        if str(role) not in ["1", "2", "3"]:
+            raise HttpError(400, "不存在的用户角色")
+        if not admin_range:
+            raise HttpError(400, "请输入管理范围")
+
+        # TODO:检查修改添加人的管理员权限
+        # privilege =
+
+        new_account = rpc.invbox.add_account(name, mobile, password, int(role), admin_range, info_list)
+        if new_account.get("resultCode"):
+            raise HttpError(400, "%s" % new_account.get("resultMsg"))
+        return jsonify(new_account)
+
+    # @login_required
+    def delete(self):
+        rdata = RequestData()
+        data = rpc.invbox.delete_account(rdata.array)
+
+        if data["resultCode"]:
+            raise HttpError(400, data["resultMsg"])
+
+        return jsonify(data)
+
+    # @login_required
+    def put(self):
+        rdata = RequestData()
+
+        params = []
+        for d in rdata:
+            id = d.get("id")
+            mobile = d.get("mobile")
+            name = d.get("name")
+            new_pwd = d.get("password")
+            com_pwd = d.get("comPwd")
+            role = d.get("role")
+            admin_range = d.get("range")
+            info_list = d.get("infoList")
+            if mobile:
+                if not is_mobile(d["mobile"]):
+                    raise HttpError(400, "手机号码格式不正确")
+            if new_pwd:
+                if not new_pwd == com_pwd:
+                    raise HttpError(400, "两次输入密码不一致")
+            if role:
+                if int(role) not in [1, 2, 3]:
+                    raise HttpError(400, "不存在的角色")
+            params.append({
+                "id": id,
+                "name": name,
+                "mobile": mobile,
+                "password": new_pwd,
+                "role": int(role),
+                "admin_range": admin_range,
+                "info_list": info_list
+            })
+        res = rpc.invbox.modify_accounts(params)
+
+        if res["resultCode"]:
+            raise HttpError(400, res["resultMsg"])
+        return jsonify(res)
+
+
 api.add_url_rule('/admin/supplyer', view_func=SupplyerAPI.as_view('supplers'))
 api.add_url_rule('/admin/user', view_func=UserAPI.as_view('users'))
 api.add_url_rule('/admin/usergroup', view_func=UserGroupAPI.as_view('usergroups'))
+api.add_url_rule('/admin/account', view_func=AccountAPI.as_view('accounts'))
